efad2bc52d4d0282f4b1a25bedde347c253149b9
Taylor Stallcup <tstallcup321@gmail.com>
2024-05-28 15:02:56 -0500

    about template created

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,7 @@ from . import views
 # holds each route we define for main_app
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('/finches', views.finches_index, name='index'),
 ]
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Flick', 'species': 'zebra finch', 'description': 'energetic and lively', 'age': 1},
+  {'name': 'Peep', 'species': 'society finch', 'description': 'sociable and friendly', 'age': 2},
+  {'name': 'Tweet', 'species': 'gouldian finch', 'description': 'colorful and vibrant', 'age': 3},
+  {'name': 'Chirp', 'species': 'star finch', 'description': 'calm and peaceful', 'age': 2},
+  {'name': 'Flutter', 'species': 'spice finch', 'description': 'curious and playful', 'age': 1}
+]
+
 # Create your views here.
 
 # Define home view
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
