e44eae4eca66db81936ae81ea09ce6459294f744
Taylor Stallcup <tstallcup321@gmail.com>
2024-05-29 13:37:34 -0500

    settings.py edited to the correct database :')

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'finchcollector.wsgi.application'
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'catcollector',
+    'NAME': 'finchcollector',
     'USER': os.environ['DB_USER'],
     'PASSWORD': os.environ['DB_PW'],
     'HOST': os.environ['DB_HOST'],
