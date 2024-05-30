7c73425310906940884a4f15f3225e1d3800fb54
Taylor Stallcup <tstallcup321@gmail.com>
2024-05-30 15:53:10 -0500

    Registered Feeding model to admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Import models to register admin
-from .models import Finch
+from .models import Finch, Feeding
 
 # Register your models here.
 admin.site.register(Finch)
+admin.site.register(Feeding)
