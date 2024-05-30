1d6e20ee506a58d5ceeff23586ba84a83d3de651
Taylor Stallcup <tstallcup321@gmail.com>
2024-05-30 15:51:09 -0500

    Created Feeding table with 1:M relationship to Finch

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 # Create your models here.
 
 class Finch(models.Model):
@@ -14,3 +20,23 @@ class Finch(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'finch_id': self.id})
+    
+# Define Feeding Model
+class Feeding(models.Model):
+    # the first optional positional argument overrides the field label
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+        )
+    
+    # Create Foreign Key
+    finch = models.ForeignKey( Finch, on_delete=models.CASCADE )
+    # 1st is the model is belongs to, then on cascade delete
+    
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
